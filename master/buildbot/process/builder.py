040d8c7262465c99bffccb5bc3373724d74a4ff8
Przemysław Warchoł <przemyslaw.warchol@stxnext.pl>
2018-05-11 11:05:37 +0200

    CDSK-920 - use better English in comment

--- master/buildbot/process/builder.py
+++ master/buildbot/process/builder.py
@@ -774,7 +774,7 @@ class BuilderControl:
     @defer.inlineCallbacks
     def rebuildBuild(self, bs, reason="<rebuild, no reason given>", extraProperties=None, absolute=True, newOwner=''):
         """
-        TODO: This method is probably is not used. Consider to remove it.
+        TODO: This method is not used probably. Consider to remove it.
         """
         klog.err_json("rebuildBuild_still_works. Don't remove me")
         if not bs.isFinished():
